51dd3ab305668302d8750b7e70b4e1d935dc2c07
石川 雄基 / ほと <yuuki.ishikawa@gaiax.com>
2015-11-27 20:56:32 +0900

    fetch image list from images.json

--- assets/app.js
+++ assets/app.js
@@ -1,24 +1,32 @@
 Array.prototype.shuffle = function() { var i, j, temp; arr = this.slice(); i = arr.length; if (i === 0) { return arr; } while (--i) { j = Math.floor(Math.random() * (i + 1)); temp = arr[i]; arr[i] = arr[j]; arr[j] = temp; } return arr; };
 
-var n = 43; // 画像の枚数
-
-var count = 0;
-var onLoadImg = function() {
-  if ( ++count === n ) {
+var count;
+function onLoadImg() {
+  if ( --count === 0 ) {
     new Masonry(document.body, { itemSelector: '.item' });
     document.body.className = '';
   }
 };
 
-var arr = [];
-for (var i=1; i <= n; i++) { arr.push( ('00'+i).slice(-3) ); }
+function fetchImages(images) {
+  var div, img;
+  count = images.length;
+  images.shuffle().map(function(name) {
+    img = document.createElement('img');
+    img.src = './images/' + name;
+    img.className = 'item';
+    img.onload = onLoadImg;
+    img.onerror = onLoadImg;
+    document.body.appendChild( img );
+  });
+}
 
-var div, img;
-arr.shuffle().map(function(name) {
-  img = document.createElement('img');
-  img.src = './images/' + name + '.jpg';
-  img.className = 'item';
-  img.onload = onLoadImg;
-  img.onerror = onLoadImg;
-  document.body.appendChild( img );
-});
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'images.json', true);
+xhr.onreadystatechange = function() {
+  if ( xhr.readyState === 4 && xhr.status === 200) {
+    var images = JSON.parse(xhr.responseText);
+    fetchImages(images.general);
+  }
+};
+xhr.send();
